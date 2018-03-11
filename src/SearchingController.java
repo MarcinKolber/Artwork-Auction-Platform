fe059ee681f8adb21f0722512f9ddec588dd7dfe
ElMarko1 <marcin.kolber1@gmail.com>
2018-03-11 02:59:19 +0000

    searching fully working

--- src/SearchingController.java
+++ src/SearchingController.java
@@ -1,13 +1,13 @@
-import java.awt.*;
 import java.util.ArrayList;
 
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 
 
 public class SearchingController {
@@ -16,7 +16,7 @@ public class SearchingController {
 	private HBox searchingTab;
 
 	@FXML
-	private Pane s;
+	private FlowPane s;
 
 	@FXML
 	private Button searchButton;
@@ -25,16 +25,49 @@ public class SearchingController {
 	private TextField searchingTextField;
 
 	public void initialize() {
+		ArrayList<Artwork> arts = FileReader.getArtworks();
+		
+		searchingTextField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				s.getChildren().clear();
+				String userInput = searchingTextField.getText();
+				
+			
+				
+				for(Artwork artwork: arts) {
+					if(artwork.getTitle().toLowerCase().contains(userInput.toLowerCase())) {
+						System.out.println(userInput);
+						Listing l = new Listing(artwork);
+						s.getChildren().add(l);
+					}
+				}
+				
+				
+			}
+
+			
+		});
 
 		searchButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
 				String userInput = searchingTextField.getText();
 				System.out.println(userInput);
+				
+			
+				
+				for(Artwork artwork: arts) {
+					if(artwork.getTitle().toLowerCase().contains(userInput.toLowerCase())) {
+						Listing l = new Listing(artwork);
+						s.getChildren().add(l);
+					}
+				}
+				
+				
 			}
 		});
 
-		ArrayList<Artwork> arts = FileReader.getArtworks();
 		for (Artwork a : arts) {
 			if (a.getTitle().contains("Test")) {
 				System.out.println(a.getTitle());
