afeccc847dc3a30260ba41d65460f46890bf86c9
ElMarko1 <marcin.kolber1@gmail.com>
2018-03-18 17:27:51 +0000

    comments

--- src/NewBidController.java
+++ src/NewBidController.java
@@ -4,36 +4,54 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 
+/**
+ * A controller for displaying new bids on artworks 
+ * @author 869527
+ *
+ */
 public class NewBidController {
 
 	@FXML
-	private FlowPane main;
-
+	private FlowPane main;	// container with artworks
 
     @FXML
-    private Label date;
+    private Label date;		// time of the last login
 	
+    // Array lists 
 	private static ArrayList<Bid> bids = new ArrayList<>();
 	private ArrayList<BidView> bidViews = new ArrayList<>();
 
+	/**
+	 * Initialise the window
+	 */
 	public void initialize() {
 
+		// Display time of the last login
 		date.setText(Updates.getDate()+"");
+		
+		// Get bids and add them to a list of bid views
 		for (Bid bid : bids) {
-
 			BidView bv = new BidView(bid);
 			bidViews.add(bv);
-
 		}
 		
+		// Display new bids in a window
 		main.getChildren().addAll(bidViews);
 
 	}
 
+	/**
+	 * Returns a list of bids
+	 * @return list of bids
+	 */
 	public ArrayList<Bid> getBids() {
 		return bids;
 	}
 
+	/**
+	 * Sets a list of bids
+	 * @param bids1 list of bids
+	 */
 	public static void setBids(ArrayList<Bid> bids1) {
 		bids = bids1;
 	}
